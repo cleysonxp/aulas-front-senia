07dec32e326fb1aeb57439bdb684e6504b906193
CLEYSON APARECIDO RIBEIRO SOARES <19267270@CFP127ALUNO.LOCAL>
2020-02-06 08:15:13 -0300

    Meu primeiro commit

---
+++ 01-Aula/script.js
@@ -0,0 +1,27 @@
+// const nome = "Cleyson";
+// const idade = 19;
+// const teste = true;
+// const produto = {produto: "mouse", quantidade:"45"};
+// const somar = () => 3 + 3; 
+
+// console.log( typeof ( nome ));
+// console.log( typeof ( idade ));
+// console.log( typeof ( teste ));
+// console.log( typeof ( produto ));
+// console.log( typeof ( somar ));
+
+// const cidade = "São Roque";
+// const estado = 'SP';
+// const pais = `BR${estado}`;
+
+// console.log("Minha cidade é:",cidade );
+// console.log("Estado:",estado );
+// console.log("Pais:", pais );
+
+// let nota1 = 8;
+// let nota2 = 8;
+
+// console.log(nota1++);
+// // console.log(nota1);
+// console.log(++nota2);
+// // nota1 += 9;
---
+++ 01-Aula/script2.js
@@ -0,0 +1,29 @@
+
+// const $nome = document.querySelector("#nome");
+// const $nome = document.querySelectorAll("#nome");
+
+const $calcular = document.getElementById('calcular');
+
+function calcularMedia(){
+    const $nome = document.getElementById('nome');
+    const $nota1 = document.getElementById('nota1');
+    const $nota2 = document.getElementById('nota2');
+    const $media = document.getElementById('media');
+    const $situacao = document.getElementById('situacao');
+
+    const media = (parseInt ($nota1.value) + parseInt ($nota2.value))/2;
+
+    if(media >= 5 ){
+        $situacao.value ="Aproado";
+        $situacao.classList.remove('reprovado');
+        $situacao.classList.add('aprovado');
+    }else{
+        $situacao.value ="Reprovado";
+        $situacao.classList.remove('aprovado');
+        $situacao.classList.add('reprovado');
+    }
+
+    $media.value = media;
+}
+
+$calcular.addEventListener('click', calcularMedia);
