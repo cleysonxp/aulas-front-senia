607f5402f7350113b865447b2a7f5b4ce74d1821
cleysonxp <cleyson.ribeiro.777@gmail.com>
2020-03-25 19:12:08 -0300

    Uando API de cep

---
+++ 07.1-postmon/script.js
@@ -0,0 +1,32 @@
+"use stritc";
+
+const $cep = document.getElementById('cep');
+
+const verificarCep = () =>  $cep.reportValidity();
+
+const encontrarCep = async ( cep ) =>{
+
+    if(verificarCep () ) {
+        const url = `https://api.postmon.com.br/v1/cep/${cep}`; 
+        const getAddress = await fetch ( url );
+        const address = await getAddress.json();
+        preencerFormulario ( address )
+    }
+}
+
+const preencerFormulario = ( endereco ) =>{
+    document.getElementById('endereco').value = endereco.logradouro;
+    document.getElementById('bairro').value = endereco.bairro;
+    document.getElementById('cidade').value = endereco.cidade;
+    document.getElementById('estado').value = endereco.estado;
+}
+
+const maskCep = ( $el ) => {
+    let aux = $el.value;
+    aux = aux.replace (/[^0-9]/g, '');
+    aux = aux.replace (/(.{5})(.)/,'$1-$2')
+    $el.value = aux;
+}
+
+$cep.addEventListener('blur', ( e ) => encontrarCep(e.target.value));
+$cep.addEventListener('keyup', ( e ) => maskCep (e.target));                           
---
+++ 07.1-postmon/teste.js
@@ -0,0 +1,33 @@
+"use stritc";
+const verificaCep = () => document.getElementById('cep').reportValidity();
+
+
+const encontrarCep = async (cep) => {
+
+    if(verificaCep()){
+    const url = `https://api.postmon.com.br/v1/cep/${cep}`;
+
+    const getAddress = await fetch (url);
+    const address = await getAddress.json();
+    preencherFormulario(address);
+    }
+}
+
+const preencherFormulario = (endereco) => {
+    document.getElementById('endereco').value = endereco.logradouro;
+    document.getElementById('bairro').value = endereco.bairro;
+    document.getElementById('cidade').value = endereco.cidade;
+    document.getElementById('estado').value = endereco.estado;
+}
+
+const maskCep = ($el) => {
+    let aux = $el.value;
+    aux = aux.replace ( /[^0-9]/g, '' );
+    aux = aux.replace ( /(.{5})(.)/, '$1-$2' );
+    $el.value = aux;
+}
+
+// "blur" - quando sair da caixinha de texto
+document.getElementById('cep').addEventListener('blur', (evento) => encontrarCep(cep.value));
+
+document.getElementById('cep').addEventListener('keyup', (evento) => maskCep(evento.target));
