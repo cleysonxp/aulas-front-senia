a46dfda9be9aba36d98e3e2bfd0bc3606424fcfe
cleysonxp <cleyson.ribeiro.777@gmail.com>
2020-04-01 13:36:17 -0300

    Casos De Corona Virus No Brasil

--- 07.0-viaCep/viacep.js
+++ 07.0-viaCep/viacep.js
@@ -2,16 +2,21 @@
 
 const encontrarCep = ( cep ) =>{
     const url = `https://viacep.com.br/ws/${cep}/json`;
-    
     fetch ( url ).then ( res => res.json())
-             .then ( data => preencerFormulario ( data ));
+             .then ( data => preencerFormulario ( data ))
 }
 
 const preencerFormulario = ( endereco ) =>{
-    document.getElementById('endereco').value = endereco.logradouro;
-    document.getElementById('bairro').value = endereco.bairro;
-    document.getElementById('cidade').value = endereco.localidade;
-    document.getElementById('estado').value = endereco.uf;
+    if (endereco != undefined){
+        document.getElementById('endereco').value = endereco.logradouro;
+        document.getElementById('bairro').value = endereco.bairro;
+        document.getElementById('cidade').value = endereco.localidade;
+        document.getElementById('estado').value = endereco.uf;    
+    }
+    else{
+        alert('teste')
+    }
+    
 }
 
 const $cep = document.getElementById('cep');
