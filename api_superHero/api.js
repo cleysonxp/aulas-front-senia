7181b4a925b5df537b1f5a33bf60d3dd454335bf
cleysonxp <cleyson.ribeiro.777@gmail.com>
2020-05-21 12:03:22 -0300

    atualização

--- api_superHero/api.js
+++ api_superHero/api.js
@@ -4,6 +4,8 @@ const heroInfo = async() => {
     const url = 'https://www.superheroapi.com/api.php/2702658359969943/search/batman';
     const getApi = await fetch( url );
     const json = await getApi.json();
+//     const array = json.results;
+//     const api = array[0];
 
-    console.log(json)
+//     console.log(api)
 }
